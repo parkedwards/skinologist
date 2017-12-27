2e65977999e1d3f6fd997fb69925a6c84bd957d0
EDWARD PARK <ed.sh.park@gmail.com>
2017-12-26 23:53:58 -0800

    adding to initial data dump logic

--- loadDB.js
+++ loadDB.js
@@ -7,29 +7,36 @@ require('dotenv').config();
 
 const db = require('./server/db');
 
-// db.query(`DROP TABLE ingredients`);
-
+// tables
 (async () => {
   try {
+    // transaction
     await db.query('BEGIN');
+
     await db.query(`
       CREATE TABLE IF NOT EXISTS ingredients (
+        _id SERIAL PRIMARY KEY,
+        name VARCHAR(80),
+        what_is_it JSONB,
+        key_benefits JSONB,
+        side_effects JSONB,
+        how_to_wear JSONB,
+        who_can_use JSONB
+      )
+    `);
+
+    await db.query(`
+      CREATE TABLE IF NOT EXISTS categories (
         _id SERIAL PRIMARY KEY,
         name VARCHAR(80)
       )
     `);
+
     await db.query(`
-      CREATE TABLE IF NOT EXISTS ing_tags (
+      CREATE TABLE IF NOT EXISTS ing_cat (
         _id SERIAL PRIMARY KEY,
         ing_id INTEGER,
-        tag_id INTEGER
-      )
-    `);
-    await db.query(`
-      CREATE TABLE IF NOT EXISTS tags (
-        _id SERIAL PRIMARY KEY,
-        ing_id INTEGER,
-        tag_id INTEGER
+        cat_id INTEGER
       )
     `);
     await db.query('COMMIT');
@@ -39,16 +46,38 @@ const db = require('./server/db');
   }
 })();
 
+// categories
 (async () => {
   try {
     const readFile = promisify(fs.readFile);
-    const data = await readFile(path.join(__dirname, '/data.json'), 'utf8');
-    console.log(data);
-    await db.query('BEGIN');
+    const data = JSON.parse(await readFile(path.join(__dirname, '/data.json'), 'utf8'));
 
-    await db.query('COMMIT');
+    const preFormatted = data
+      .map(ing => ing['Product Category'] || null)
+      .filter(arr => arr != null && arr[0] !== '')
+      .reduce((arr, chunk) => [...arr, ...chunk], []);
+
+    // unique
+    const categories = Array.from(new Set(preFormatted));
+
+    categories.forEach(async category => {
+      const query = {
+        text: 'INSERT INTO categories(name) VALUES($1)',
+        values: [category],
+      };
+
+      try {
+        await db.query(query);
+        console.log('category inserted successfully'.green);
+        console.log(category);
+      } catch (error) {
+        console.log('----------------------------------------->');
+        console.log('issue inserting category'.red);
+        console.log(error);
+        console.log(category);
+      }
+    });
   } catch (error) {
-    console.log('<----- ERROR with INSERTS ----->'.bold.red);
-    await db.query('ROLLBACK');
+    console.log('<----- ERROR with categories INSERTS ----->'.bold.red);
   }
 })();
