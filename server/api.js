f06c46979f32c57ea642b41044c181ba88b322d0
EDWARD PARK <ed.sh.park@gmail.com>
2017-12-30 23:08:18 -0800

    completing fetch api

--- server/api.js
+++ server/api.js
@@ -1,26 +1,18 @@
 const { Router } = require('express');
 const Sifter = require('sifter');
 const redis = require('redis');
+const chalk = require('chalk');
+
+const { log } = console;
+const db = require('./db');
 
 const { REDIS_URL } = process.env;
 const redis_client = redis.createClient(REDIS_URL);
+
 // handle redis exceptions
 redis_client.on('error', err => {
   console.log(`Error ${err}`);
 });
-
-// placeholder stuff
-const path = require('path');
-const fs = require('fs');
-const { promisify } = require('util');
-
-let data;
-let sifter;
-(async () => {
-  const readFile = promisify(fs.readFile);
-  data = JSON.parse(await readFile(path.join(__dirname, '../data.json')), 'utf8');
-  sifter = new Sifter(data);
-})();
 
 const api = new Router();
 
@@ -38,49 +30,67 @@ api.get(
   (req, res, next) => {
     const searchTerm = req.query.term;
     if (!searchTerm) {
-      return res.status(404).end('Page Not Found');
-      // return res.redirect('/404'); // check out express docs for more cannonical error handling
+      return res.status(404).end('invalid query');
     }
     next();
   },
   fetchItemsFromCache,
   queryDB,
-  sendResult,
+  searchAndSend,
 );
 
 module.exports = api;
 
 function fetchItemsFromCache(req, res, next) {
-  // const searchTerm = req.query.term;
   const ingredientsList = 'ingredientsList';
+
   redis_client.get(ingredientsList, (err, cachedData) => {
     if (err) console.log(err); // it's ok to pass through to query DB if error with redis retrieve
-    if (cachedData != null) res.locals.ingredients = cachedData;
-    next();
+    if (cachedData != null) {
+      log(chalk.green('[ -------- FETCHING ingredients FROM REDIS  -------- ]'));
+      res.locals.ingredients = cachedData;
+    }
+
+    return next();
   });
 }
 
-function queryDB(req, res, next) {
+async function queryDB(req, res, next) {
   if (!res.locals.ingredients) {
-    // db query here
-    // also cache the return data
-    return next();
+    // query db
+    log(chalk.magenta('[ -------- QUERYING postgres FOR INGREDIENTS  -------- ]'));
+    const { rows } = await db.query('SELECT * FROM ingredients');
+    const stringPayload = JSON.stringify(rows);
+
+    // cache results
+    log(chalk.blue('[ -------- RE-CACHING query results  -------- ]'));
+    redis_client.setex('ingredientsList', 3600, stringPayload);
+
+    // add payload to 'locals' key of response cycle
+    res.locals.ingredients = stringPayload;
   }
   // if cached data has already been added to res.locals
-  next();
+  return next();
 }
 
-function sendResult(req, res, next) {
+function searchAndSend(req, res) {
+  const { ingredients } = res.locals;
+  const parsedData = JSON.parse(ingredients);
+
   const searchTerm = req.query.term;
 
+  log(chalk.yellow('[ -------- Instantiating Sifter  -------- ]'));
+  // can you move this Sifter instantiation so that you don't have to do it every time?
+  const sifter = new Sifter(parsedData);
   const { items: scores } = sifter.search(searchTerm, {
-    fields: ['Name'],
+    fields: ['name'],
     sort: [{ field: 'name', direction: 'asc' }],
     conjunction: 'and',
     limit: 100,
   });
 
-  const results = scores.map(score => data[score.id]);
+  // sifter give back array indices, so need to map back to original data set
+  const results = scores.map(score => parsedData[score.id]);
 
   return res.status(200).json(results);
 }
