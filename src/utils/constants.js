5894c3745ae08c9d919ebcfb9cbfb0885d08dfee
EDWARD PARK <ed.sh.park@gmail.com>
2018-01-06 18:52:59 -0800

    creating a section constants array

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+// this array determines order
+export const sections = [
+  { display: 'What is it?', key: 'what_is_it', icon: 'place' },
+  { display: 'Symptoms', key: 'symptoms', icon: 'local_hospital' },
+  { display: 'Key Benefits', key: 'key_benefits', icon: 'favorite_border' },
+  { display: 'Side Effects', key: 'side_effects', icon: 'flash_on' },
+  { display: 'How to Wear', key: 'how_to_wear', icon: 'info_outline' },
+  { display: 'Who Can Use', key: 'who_can_use', icon: 'person_pin' },
+];
